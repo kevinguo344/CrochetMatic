fde9c004655c288c32a423ff8d4ce3dc97f95c7b
Kevin Guo <kg344@cornell.edu>
2018-02-13 12:18:58 -0500

    Convert unit needle sequence to i2c

---
+++ Unit_needle_i2c/Unit_needle_i2c.ino
@@ -0,0 +1,17 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define SERVOMIN
+#define SERVOMAX
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
