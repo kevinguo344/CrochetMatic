0d22f7d8b157505eafdce8383a090d8a79eaaa7b
Kevin Guo <kg344@cornell.edu>
2018-02-13 19:55:55 -0500

    Adjusted SERVOMIN, SERVOMAX

--- Unit_needle_i2c/Unit_needle_i2c.ino
+++ Unit_needle_i2c/Unit_needle_i2c.ino
@@ -3,8 +3,8 @@
 
 Adafruit_PWMServoDriver driver = Adafruit_PWMServoDriver();
 
-#define SERVOMIN 500
-#define SERVOMAX 2400
+#define SERVOMIN 150
+#define SERVOMAX 600
 
 uint8_t servonum = 2;
 const int topServo = 0;
@@ -18,14 +18,14 @@ void setup() {
   rest();
 }
 
+long convertAngle(int a){
+  return map(a, 0, 180, SERVOMIN, SERVOMAX);
+}
+
 void loop() {
   //rest();
   sequence();
   //spin();
-}
-
-long convertToSeconds(int a){
-  return map(a, 0, 180, SERVOMIN, SERVOMAX);
 }
 
 void sequence(){
@@ -35,43 +35,30 @@ void sequence(){
 }
 
 void rest(){
-  setServoPulse(topServo, convertToSeconds(0));
-  setServoPulse(botServo, convertToSeconds(180));
+  driver.setPWM(topServo, 0, convertAngle(0));
+  driver.setPWM(botServo, 0, convertAngle(180));
 }
 
 void spin(){
   for(int i=0; i<180; i+=1){
-    setServoPulse(topServo, convertToSeconds(i));
-    setServoPulse(botServo, convertToSeconds(180-i));
-    delay(15);
+    driver.setPWM(topServo, 0, convertAngle(i));
+    driver.setPWM(botServo, 0, convertAngle(180-i));
+    delay(5);
   }
 
   for(int i=179; i>=0; i-=1){
-    setServoPulse(topServo, convertToSeconds(i));
-    setServoPulse(botServo, convertToSeconds(180-i));
-    delay(15);
+    driver.setPWM(topServo, 0, convertAngle(i));
+    driver.setPWM(botServo, 0, convertAngle(180-i));
+    delay(5);
   }
 }
 
 void pullDown(){
-  setServoPulse(topServo, convertToSeconds(150));
-  setServoPulse(botServo, convertToSeconds(60));
+  driver.setPWM(topServo, 0, convertAngle(150));
+  driver.setPWM(botServo, 0, convertAngle(60));
 }
 
 void closeUp(){
-  setServoPulse(topServo, convertToSeconds(0));
-  setServoPulse(botServo, convertToSeconds(75));
-}
-
-void setServoPulse(uint8_t n, double pulse) {
-  double pulselength;
-  pulselength = 1000000;   // 1,000,000 us per second
-  pulselength /= 60;   // 60 Hz
-  Serial.print(pulselength); Serial.println(" us per period"); 
-  pulselength /= 4096;  // 12 bits of resolution
-  Serial.print(pulselength); Serial.println(" us per bit"); 
-  pulse *= 1000;
-  pulse /= pulselength;
-  Serial.println(pulse);
-  driver.setPWM(n, 0, pulse);
+  driver.setPWM(topServo, 0, convertAngle(0));
+  driver.setPWM(botServo, 0, convertAngle(75));
 }
