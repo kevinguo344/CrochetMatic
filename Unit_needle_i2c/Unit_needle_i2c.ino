40769ee70fe33b6de880149b41ca3148cfaaae5c
Kevin Guo <kg344@cornell.edu>
2018-02-21 13:23:36 -0500

    Enabled i2c experimental

--- Unit_needle_i2c/Unit_needle_i2c.ino
+++ Unit_needle_i2c/Unit_needle_i2c.ino
@@ -6,6 +6,30 @@ Adafruit_PWMServoDriver driver = Adafruit_PWMServoDriver();
 #define SERVOMIN 150
 #define SERVOMAX 600
 
+/*
+example output
+//GCODE SETUP
+M260 A64
+
+//REST
+M280 P1 S150
+M280 P2 S600
+
+//DELAY 500
+G4 P500
+
+//PULL DOWN
+M280 P1 S525
+M280 P2 S300
+
+//DELAY 500
+G4 P500
+
+//CLOSE UP
+M280 P1 S150
+M280 P2 S312.5
+*/
+
 uint8_t servonum = 2;
 const int topServo = 0;
 const int botServo = 1;
@@ -16,9 +40,11 @@ void setup() {
   driver.setPWMFreq(60);
   yield();
   rest();
+  Serial.println("M260 A64");
 }
 
 long convertAngle(int a){
+  Serial.println(map(a, 0, 180, SERVOMIN, SERVOMAX));
   return map(a, 0, 180, SERVOMIN, SERVOMAX);
 }
 
@@ -38,7 +64,9 @@ void sequence(){
 
 void rest(){
   driver.setPWM(topServo, 0, convertAngle(0));
+  Serial.println("M280 P1 S150");
   driver.setPWM(botServo, 0, convertAngle(180));
+  Serial.println("M280 P2 S600");
 }
 
 void spin(){
@@ -57,10 +85,14 @@ void spin(){
 
 void pullDown(){
   driver.setPWM(topServo, 0, convertAngle(150));
+  Serial.println("M280 P1 S525");
   driver.setPWM(botServo, 0, convertAngle(60));
+  Serial.println("M280 P2 S300");
 }
 
 void closeUp(){
   driver.setPWM(topServo, 0, convertAngle(0));
+  Serial.println("M280 P1 S150");
   driver.setPWM(botServo, 0, convertAngle(65));
+  Serial.println("M280 P2 S312.5");
 }
