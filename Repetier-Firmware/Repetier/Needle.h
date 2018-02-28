6d7cf8edf95fb850b030e27fa0bc1c2b291615d7
Kevin Guo <kg344@cornell.edu>
2018-02-28 14:13:18 -0500

    IT COMPLIES

--- Repetier-Firmware/Repetier/Needle.h
+++ Repetier-Firmware/Repetier/Needle.h
@@ -5,30 +5,33 @@
 #define SERVOMIN 150
 #define SERVOMAX 600
 
-std::vector<uint8_t> driverAddress;
-std::vector<Adafruit_PWMServoDriver:Adafruit_PWMServoDriver> drivers;
+//std::vector<uint8_t> driverAddress;
+//std::vector<Adafruit_PWMServoDriver:Adafruit_PWMServoDriver> drivers;
+
+class Motor{
+  public:
+		Motor(uint8_t addr, int i, bool inv);
+		uint8_t setDriverAddress(uint8_t a);
+		int setMotorIndex(int i);
+		bool setInverted(bool b);
+  private:
+		uint8_t driverAddress;
+		int index;
+		bool inverted;
+		Adafruit_PWMServoDriver driver;
+};
 
 class Needle{
 	public:
-		Motor topMotor;
-		Motor bottomMotor
 		Needle(Motor t, Motor b);
 		void initializeDriver();
 		void rest();
 		void closeUp();
 		void pullDown();
 		void sequence();
-}
+	private:
+		Motor topMotor;
+		Motor bottomMotor;
+};
 
-class Motor{
-	public:
-		Adafruit_PWMServoDriver *driver;
-		uint8_t driverAddress;
-		int index;
-		bool inverted;
-		Motor();
-		Motor(uint8_t addr, int i, bool inv);
-		uint8_t setDriverAddress(uint8_t a);
-		int setMotorIndex(int i);
-		bool setInverted(bool b);
-}
+
